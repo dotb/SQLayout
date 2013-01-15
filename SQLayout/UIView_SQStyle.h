7344a1385d971342818b8c6d22ad5b134f6add05
Bradley Clayton <bradley@squarepolka.com>
2013-01-15 14:01:01 +1100

    /Users/bradley/Documents/Sause/DeloitteAus/GEM Prototype/GEM Prototype/GEM Prototype/3rd Party Libraries/Squarepolka/SQLayout/SQLayout/UIView_SQStyle.h

---
+++ SQLayout/UIView_SQStyle.h
@@ -0,0 +1,13 @@
+//
+//  UIView_SQStyle.h
+//  GEM Prototype
+//
+//  Created by Bradley Clayton on 15/01/13.
+//  Copyright (c) 2013 GE Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView ()
+
+@end
